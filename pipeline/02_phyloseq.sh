bc598e443743fdf776ababc4f0432c2b09474e22
Jason Stajich <jasonstajich.phd@gmail.com>
2023-09-12 00:43:19 -0700

    resize

--- pipeline/02_phyloseq.sh
+++ pipeline/02_phyloseq.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash -l
-#SBATCH -p short -C xeon -N 1 -n 2 --mem 2gb
+#SBATCH -p short -N 1 -c 2 -n 1 --mem 16gb --out logs/plot_figures.log
 
 module load R
 
-Rscript -e 'library(rmarkdown); rmarkdown::render("scripts/plot_ampliconSummary.Rmd", "html_document")'
+#Rscript -e 'library(rmarkdown); rmarkdown::render("scripts/plot_ampliconSummary.Rmd", "html_document")'
+Rscript scripts/plot_amplicon_simple.R
